4392717c1372cd73d046579f9ea3e1f5775bef69
Jose Osorio <joseinvictus273@hotmail.com>
2022-12-05 15:58:00 -0500

    Adding the folder for the new tasks

---
+++ Day3/findAChair.js
@@ -0,0 +1,28 @@
+let meeting = (meetRooms, chairsNeeded) =>{
+    if(chairsNeeded === 0) return 'Game on'
+
+    const avalCha = meetRooms.map(([occupChairs, totalChairs]) => (occupChairs.length >= totalChairs) ? 0 : totalChairs - occupChairs.length)
+    const totalAvalChairs = avalCha.reduce((a,b) => a + b, 0)
+
+    if(totalAvalChairs < chairsNeeded) return 'Not enough'
+
+    let totalSparedChairs = 0
+    const spareChairs = []
+    
+    for(let i = 0; i < avalCha.length; i++){
+        if(totalSparedChairs === chairsNeeded) return spareChairs
+        totalSparedChairs += avalCha[i]
+        spareChairs.push(avalCha[i])
+    }
+
+    return spareChairs
+}
+
+
+console.log(meeting([['XXX', 3], ['XXXXX', 6], ['XXXXXX', 9]], 4)) // ---> [0, 1, 3]
+
+console.log(meeting([['XXX', 1], ['XXXXXX', 6], ['X', 2], ['XXXXXX', 8], ['X', 3], ['XXX', 1]], 5)) //---> [0, 0, 1, 2, 2]
+
+console.log(meeting([['XX', 2], ['XXXX', 6], ['XXXXX', 4]], 0)) // ---> 'Game On'
+
+console.log(meeting([['XX', 2], ['XXXX', 6], ['XXXXX', 4]], 4)) // ---> 'Not enough!'
